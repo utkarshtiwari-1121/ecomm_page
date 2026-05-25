5339498decbf32987a5ac3fbda6fc3cfd7b5e89d
utkarsh <utkarshtiwari.1121@gmail.com>
2026-05-25 19:35:40 +0530

    top selling

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".heroSwiper", {
   pagination: {
-    el: ".swiper-pagination",
+    el: ".hero_banner",
     clickable: true,
   },
+});
+
+var swiper = new Swiper(".shop_by_category_swiper", {
+  slidesPerView: 8,
+  loop: true,
+  spaceBetween: 20, 
+  // cssMode: true,
+  cssMode: false, 
+  navigation: {
+    nextEl: "#next_button",
+    prevEl: "#previous_button",
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+  mousewheel: true,
+  keyboard: true,
 });
