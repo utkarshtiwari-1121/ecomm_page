81bb7d8e2a699d70be82f9bf2237a5ccf87dfb2e
utkarsh <utkarshtiwari.1121@gmail.com>
2026-05-26 19:25:22 +0530

    laptop

--- script.js
+++ script.js
@@ -8,16 +8,58 @@ var swiper = new Swiper(".heroSwiper", {
 var swiper = new Swiper(".shop_by_category_swiper", {
   slidesPerView: 8,
   loop: true,
-  spaceBetween: 20, 
-  // cssMode: true,
-  cssMode: false, 
+  spaceBetween: 20,
+  cssMode: false,
+
   navigation: {
     nextEl: "#next_button",
     prevEl: "#previous_button",
   },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
+
   mousewheel: true,
   keyboard: true,
+
+  breakpoints: {
+
+    // Mobile
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    // Large Mobile
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+
+    // Tablet
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+
+    // Small Laptop
+    1124: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+
+    // Desktop
+    1280: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+
+    // Large Desktop
+    1440: {
+      slidesPerView: 8,
+      spaceBetween: 20,
+    },
+  },
 });
